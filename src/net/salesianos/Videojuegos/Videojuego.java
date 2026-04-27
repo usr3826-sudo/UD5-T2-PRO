4c26ab3263ac29efff4020a78e5d329c7cade50c
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-04-27 10:23:13 +0200

    feat: constructor y getter y setter

--- src/net/salesianos/Videojuegos/Videojuego.java
+++ src/net/salesianos/Videojuegos/Videojuego.java
@@ -6,4 +6,43 @@ public class Videojuego {
     private String genero;
     private String plataforma;
     private int puntuacion;
+
+    public Videojuego(String nombre, String genero, String plataforma, int puntuacion) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.plataforma = plataforma;
+        this.puntuacion = puntuacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
 }
