d84b49d8a506bbea35e79d03f14d0e385de7ab63
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-04-27 09:53:20 +0200

    feat: clase Videojuego y atributos

---
+++ src/net/salesianos/Videojuegos/Videojuego.java
@@ -0,0 +1,9 @@
+package net.salesianos.Videojuegos;
+
+public class Videojuego {
+
+    private String nombre;
+    private String genero;
+    private String plataforma;
+    private int puntuacion;
+}
